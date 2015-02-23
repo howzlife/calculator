cb3da4e6810e217e95cda871495da149841cb013
howzlife <nico_dubus@hotmail.com>
2015-02-23 12:25:03 -0500

    first commit, 2 lectures in

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nicolas Dubus on 2015-02-19.
+//  Copyright (c) 2015 WorldDubination. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var userIsInTheMiddleOfTypingANumber = false
+
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+        display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+        case "✖️": performOperation { $1 * $0 }
+        case "➗": performOperation { $1 / $0 }
+        case "➕": performOperation { $1 + $0 }
+        case "➖": performOperation { $1 - $0 }
+        case "✔️": performOperation {sqrt($0)}
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double  ) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: (Double) -> Double  ) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack: /(operandStack)")
+    }
+
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+           display.text = "\(newValue)"
+           userIsInTheMiddleOfTypingANumber = false
+        }
+        
+    }
+}
+
